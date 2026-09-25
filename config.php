ecb8059efcb6700b9690488cd5019c4dc814e5fe
Quik <tryte239@gmail.com>
2026-09-25 19:24:07 +0200

    Remove API key and base URL from config.php

--- config.php
+++ config.php
@@ -1,10 +1,1 @@
-<?php
 
-// Inserisci qui la tua API Key ufficiale generata su:
-// https://developer.brawlstars.com  (sezione "My Account" -> "Create New Key")
-// IMPORTANTE: quando crei la key, devi indicare l'IP pubblico del TUO server.
-// Se il server ha IP dinamico o è dietro un proxy/CDN, la key non funzionerà
-// finché l'IP non corrisponde esattamente a quello registrato.
-define('BRAWL_API_KEY', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImM1NWRjN2RiLTAxYzEtNDk2My1iY2JhLWMxMGI3MTFlYzE4ZiIsImlhdCI6MTc5MDE2NTY4Niwic3ViIjoiZGV2ZWxvcGVyL2UyZTJlYjVjLTVjYTUtNDY2Yy1iZjNiLTI1N2Q4MTk0ZjM2NSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMi4zNC4zMC4xMjIiXSwidHlwZSI6ImNsaWVudCJ9XX0.TLohvlpwNo7zw44-DJ6ZU3mITdNE4WXymMB_lLZqOcCBAEdMXiK_5l661G6xvHwTfdZ-nyf-pAZZBk8ZjaGaSQ');
-
-define('BRAWL_API_BASE', 'https://api.brawlstars.com/v1');
